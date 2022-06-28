b349867957c54b96a86bff6ad0481305798b3e19
Hafidh Rizky <hafidhrzk@gmail.com>
2022-06-29 00:43:39 +0700

    ten commit

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = {};
 const sequelize = new Sequelize('course-express-b34', 'root', 'root', {
-  host: 'localhost' || 'https://b34-backend.herokuapp.com/api/v1',
+  host: 'localhost' || 'https://loquacious-mooncake-e00a17.netlify.app',
   dialect: 'mysql',
   logging: console.log,
   freezeTableName: true,

258d30383d45a067e59988d37da398d54f517bbb
Hafidh Rizky <hafidhrzk@gmail.com>
2022-06-29 00:24:19 +0700

    second commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "https://loquacious-mooncake-e00a17.netlify.app",
   },
 });
 
@@ -22,7 +22,7 @@ require('./src/socket')(io);
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors("https://loquacious-mooncake-e00a17.netlify.app"));
+app.use(cors());
 
 app.use('/api/v1/', router);
 app.use('/uploads', express.static('uploads'));
